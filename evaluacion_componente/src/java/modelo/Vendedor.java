b165ef4bc7ec736ac4322679b0f2d6f87067cc16
19502093-0 <19502093-0@cur_lab0308.inacap.cl>
2018-04-10 09:29:55 -0300

    Modelos: 	-Vendedor 	-Producto

---
+++ evaluacion_componente/src/java/modelo/Vendedor.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author 19502093-0
+ */
+class Vendedor {
+    private int rut;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String email;
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public void mostrarDatosVendedor(){
+        this.getRut();
+        this.getNombre();
+        this.getApellido();
+        this.getDireccion();
+        this.getEmail();
+    }
+    
+    
+}
